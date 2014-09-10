6f7c8b4e74440687a046febe0dfea5ad033a4820
Elena Reeves-Walker <elena@tchopshop.com>
2014-09-10 15:16:03 -0500

    update tpl

--- sites/all/themes/baseline/templates/views-view-unformatted--green-vendors.tpl.php
+++ sites/all/themes/baseline/templates/views-view-unformatted--green-vendors.tpl.php
@@ -12,8 +12,6 @@
   <h3><?php print $title; ?></h3>
 <?php endif; ?>
 <?php foreach ($rows as $id => $row): ?>
-  <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
     <?php print $row; ?>
-  </div>
 <?php endforeach; ?>
 </div>
