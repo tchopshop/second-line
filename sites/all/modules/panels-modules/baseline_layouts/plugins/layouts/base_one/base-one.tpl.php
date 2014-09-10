95576c9430871b90d8b2939d65e970cfa852fcc6
Elena Reeves-Walker <elena@tchopshop.com>
2014-09-10 15:04:20 -0500

    update to baseline_layouts

--- sites/all/modules/panels-modules/baseline_layouts/plugins/layouts/base_one/base-one.tpl.php
+++ sites/all/modules/panels-modules/baseline_layouts/plugins/layouts/base_one/base-one.tpl.php
@@ -1,3 +1,3 @@
-<div class="<?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<div class="<?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?> row" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
       <?php print $content['one_main']; ?>
 </div>
---
+++ sites/all/modules/panels-modules/baseline_layouts/plugins/layouts/base_one_fullwidth/base-one_fullwidth.tpl.php
@@ -0,0 +1,3 @@
+<div class="<?php if (!empty($classes)) { print $classes; } ?><?php if (!empty($class)) { print $class; } ?>" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+      <?php print $content['one_main']; ?>
+</div>
